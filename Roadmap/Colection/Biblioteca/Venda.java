15c977bf65cb79311571b7f6acdf9e919d5cb965
Gustavo Santana <gustavosantana1255@gmail.com>
2026-09-11 00:08:51 -0300

    files(Roadmap/Colections):Deque final files

---
+++ Roadmap/Colection/Biblioteca/Venda.java
@@ -0,0 +1,57 @@
+package Colection.Biblioteca;
+
+public class Venda {
+    private int id;
+    private Cliente cliente;
+    private String data;
+    private Livro livro;
+
+    public Venda(int id, String data, Livro livro) {
+        this.id = id;
+        this.data = data;
+        this.livro = livro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Venda{" +
+                "id=" + id +
+                ", cliente='" + getCliente() + '\'' +
+                ", data='" + data + '\'' +
+                ", livro=" + livro +
+                '}';
+    }
+}
